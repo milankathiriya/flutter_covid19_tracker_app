cb1464147933ed04a75f632aee23e335104ac90f
MAC <mac@MACs-MacBook-Pro.local>
2023-01-04 11:25:47 +0530

    third update

--- android/app/src/main/kotlin/com/example/covid_19_tracker_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/covid_19_tracker_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.covid_19_tracker_app
+package com.mk99.covid_19_tracker_app
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:covid_19_tracker_app/views/screens/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.light(useMaterial3: true).copyWith(
-        appBarTheme: AppBarTheme(backgroundColor: Colors.indigoAccent),
+        appBarTheme: const AppBarTheme(backgroundColor: Colors.indigoAccent),
       ),
       routes: {
         '/': (context) => const HomePage(),
